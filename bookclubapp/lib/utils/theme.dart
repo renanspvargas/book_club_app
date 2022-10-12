8cba022bf52ed6f70501da1b769051e0b26f0ffc
Renan Vargas <renanspvargas@outlook.com>
2022-10-11 21:18:29 -0300

    light green added to theme

--- bookclubapp/lib/utils/theme.dart
+++ bookclubapp/lib/utils/theme.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 class OurTheme {
+  final Color _lightGreen = const Color.fromARGB(255, 213, 235, 220);
+
   ThemeData buildTheme() {
-    return ThemeData();
+    return ThemeData(
+      canvasColor: _lightGreen,
+    );
   }
 }
