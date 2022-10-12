2adf6ed18fd89659f5fc3b2cdbf751ef6cfef8dd
Renan Vargas <renanspvargas@outlook.com>
2022-10-11 21:03:17 -0300

    login screen file created

---
+++ bookclubapp/lib/screens/login/login.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OurLogin extends StatelessWidget {
+  const OurLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(),
+    );
+  }
+}
