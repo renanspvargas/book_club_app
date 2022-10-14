0e54ba8a084f2237c948cd16e12fc7ad5dee00cd
Renan Vargas <renanspvargas@outlook.com>
2022-10-14 14:54:45 -0300

    creating OurSignUp widget screen

---
+++ bookclubapp/lib/screens/signup/signup.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OurSignUp extends StatelessWidget {
+  const OurSignUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
