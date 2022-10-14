1858e3b1c8f68709a34fbc9b4aec6b7bf552916c
Renan Vargas <renanspvargas@outlook.com>
2022-10-14 13:57:53 -0300

    correcting color

--- bookclubapp/lib/utils/theme.dart
+++ bookclubapp/lib/utils/theme.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class OurTheme {
   final Color _lightGreen = const Color.fromARGB(255, 213, 235, 220);
   final Color _lightGrey = const Color.fromARGB(255, 164, 164, 164);
-  final Color _darkerGrey = const Color.fromARGB(255, 219, 124, 135);
+  final Color _darkerGrey = const Color.fromARGB(255, 119, 124, 135);
 
   ThemeData buildTheme() {
     return ThemeData(
