d7e6613f37b69eb7bcc7b9bc497b4be75fca2747
Renan Vargas <renanspvargas@outlook.com>
2022-10-14 13:55:17 -0300

    adding colors to theme

--- bookclubapp/lib/utils/theme.dart
+++ bookclubapp/lib/utils/theme.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 
 class OurTheme {
   final Color _lightGreen = const Color.fromARGB(255, 213, 235, 220);
+  final Color _lightGrey = const Color.fromARGB(255, 164, 164, 164);
+  final Color _darkerGrey = const Color.fromARGB(255, 219, 124, 135);
 
   ThemeData buildTheme() {
     return ThemeData(
       canvasColor: _lightGreen,
+      primaryColor: _lightGrey,
+      secondaryHeaderColor: _darkerGrey,
     );
   }
 }
