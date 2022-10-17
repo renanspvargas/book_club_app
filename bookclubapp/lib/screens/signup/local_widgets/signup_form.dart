ee71b37cbb7d8f97dc6749c0df637989e46b0d6d
Renan Vargas <renanspvargas@outlook.com>
2022-10-17 16:05:57 -0300

    ui sign up form completed

--- bookclubapp/lib/screens/signup/local_widgets/signup_form.dart
+++ bookclubapp/lib/screens/signup/local_widgets/signup_form.dart
@@ -1,3 +1,4 @@
+import 'package:bookclubapp/widgets/our_container.dart';
 import 'package:flutter/material.dart';
 
 class OurSignUpForm extends StatelessWidget {
@@ -5,6 +6,70 @@ class OurSignUpForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return OurContainter(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 20,
+              horizontal: 8,
+            ),
+            child: Text(
+              "Sign Up",
+              style: TextStyle(
+                color: Theme.of(context).secondaryHeaderColor,
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.person_outline),
+              hintText: "Full name",
+            ),
+          ),
+          const SizedBox(height: 20),
+          TextFormField(
+            decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.alternate_email),
+              hintText: "Email",
+            ),
+          ),
+          const SizedBox(height: 20),
+          TextFormField(
+            decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.lock_outline),
+              hintText: "Password",
+            ),
+            obscureText: true,
+          ),
+          const SizedBox(height: 20),
+          TextFormField(
+            decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.lock_open),
+              hintText: "Confirm password",
+            ),
+            obscureText: true,
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.7,
+            height: 35,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                "SignUp",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
