5d5c22559d35df8538c8dff6ebb50188cb265c9b
Renan Vargas <renanspvargas@outlook.com>
2022-10-17 15:57:11 -0300

    correcting import

--- bookclubapp/lib/screens/signup/local_widgets/signup_form.dart
+++ bookclubapp/lib/screens/signup/local_widgets/signup_form.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class OurSignUpForm extends StatelessWidget {
   const OurSignUpForm({super.key});
